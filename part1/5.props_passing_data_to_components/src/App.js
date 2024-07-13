ed6ec5296fffabc8cd2ad24cf438c7e8c3ed80da
Telmo Fernandes <telmorf@live.com>
2024-07-13 22:01:59 +0100

    1a Introduction to React (End of section)

---
+++ part1/5.props_passing_data_to_components/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+export default App;
+
--- part1/5.props_passing_data_to_components/src/index.js
+++ part1/5.props_passing_data_to_components/src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <p>teste 1 index.jx</p>
+
   </React.StrictMode>
 );
 
