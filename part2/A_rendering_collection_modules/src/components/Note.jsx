313cc714392dc1bde810cc5807eb2f4707a9a2d9
Telmo Fernandes <telmorf@live.com>
2024-08-10 13:43:35 +0100

    part2-6_PromisesAndErrors

--- part2/A_rendering_collection_modules/src/components/Note.jsx
+++ part2/A_rendering_collection_modules/src/components/Note.jsx
@@ -1,6 +1,15 @@
-const Note = ({ note }) => {
+const Note = ({ note, toggleImportance  }) => {
+  
+  
+  const label = note.important
+    ? 'make not important' : 'make important'
+  
   return (
-    <li>{note.content}</li>
+    <li>
+      <button onClick={toggleImportance}>{label}</button>
+      {note.content}
+    </li>
+    
   )
 }
 
